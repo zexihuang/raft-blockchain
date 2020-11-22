6a43d58ec752580d24d850a09bd1538ef2c2e57a
Mert Kosan <mertkosan@gmail.com>
2020-11-23 01:13:46 +0300

    + operation

--- server.py
+++ server.py
@@ -41,19 +41,28 @@ class Server:
                 print('Wrong server name. Please enter 0, 1 or 2.')
 
         # Initialize blockchains, balance tables, proof of work working area, etc.
+
         # Server state.
         self.server_state = 'Follower'  # Follower, Leader, Candidate
         self.server_state_lock = Lock()
+
         # Server term.
         self.server_term = 0
         self.server_term_lock = Lock()
+
         # Voted candidate.
         self.voted_candidate = None
         self.voted_candidate_lock = Lock()
 
         # State variables for operation.
         self.servers_operation_last_seen = [time.time(), time.time(), time.time()]
+        self.servers_operation_last_seen_lock = Lock()
+
         self.servers_log_next_index = [0, 0, 0]
+        self.servers_log_next_index_lock = Lock()
+
+        self.commit_index = 0
+        self.commit_index_lock = Lock()
 
         # State variables for vote.
         self.last_election_time = 0
@@ -62,53 +71,70 @@ class Server:
         # State variables for client.
 
         self.blockchain = []
+        self.blockchain_lock = Lock()
         self.balance_table = []
 
     # Operation utilities.
-    def threaded_on_receive_operation(self, connection):
+    def threaded_on_receive_operation(self, msg):
         # Receive and process append request/response and heartbeat messages.
+        if msg[0] == 'Operation-Request':
+            if len(msg[3]['entries']) == 0:  # heartbeat message
+                start_new_thread(self.threaded_leader_election_watch, ())
+            else:  # append message
+                pass
 
-        pass
+        elif msg[0] == 'Operation-Response':
+            pass
+        else:
+            raise NotImplementedError(f'Header {msg[0]} is not related!')
+
 
     def threaded_response_watch(self, receiver):
         # Watch whether we receive response for a specific normal operation message sent. If not, resend the message.
-        timeout_time = random.uniform(5.0, 10.0)
-        time.sleep(timeout_time)
-        if time.time() - self.servers_operation_last_seen[receiver] > timeout_time:
-            # timed out, resend
+        timeout = random.uniform(40.0, 80.0)
+        time.sleep(timeout)
+        self.servers_operation_last_seen_lock.acquire()
+        if time.time() - self.servers_operation_last_seen[receiver] > timeout:  # timed out, resend
+            self.servers_operation_last_seen_lock.release()
             start_new_thread(self.threaded_response_watch, (receiver,))
             start_new_thread(self.threaded_send_append_request, ([receiver],))
 
-    def generate_operation_request_message(self, is_heartbeat=False):
+    def generate_operation_request_message(self, receiver, is_heartbeat=False):
         header = 'Operation-Request'
         sender = self.server_id
-        receiver = None
+
+        self.servers_log_next_index_lock.acquire()
+        self.blockchain_lock.acquire()
+        previous_log_index = self.servers_log_next_index[receiver] - 1
+        next_log_index = self.servers_log_next_index[receiver]
+        # TODO: checkout 'term' key when implement blockchain
+        previous_log_term = self.blockchain[previous_log_index]['term']
+        self.servers_log_next_index_lock.release()
+        self.blockchain_lock.release()
+
         message = {
             'term': self.server_term,
             'leader_id': self.server_id,
-            'previous_log_index': ...,
-            'previous_log_term': ...,
-            'entries': [] if is_heartbeat else [...],
-            'commit_index': ...
+            'previous_log_index': previous_log_index,
+            'previous_log_term': previous_log_term,
+            'entries': [] if is_heartbeat else self.blockchain[next_log_index:],
+            'commit_index': self.commit_index
         }
         return [header, sender, receiver, message]
 
     def threaded_send_append_request(self, receivers):
         # Send append requests to followers.
-        msg = self.generate_operation_request_message()
         for receiver in receivers:
-            msg[2] = receiver
+            msg = self.generate_operation_request_message(receiver)
             start_new_thread(self.threaded_on_receive_operation, ())
             start_new_thread(utils.send_message, (tuple(msg), Server.CHANNEL_PORT))
 
     def threaded_heartbeat(self):
         # Send normal operation heartbeats to the followers.
-        while self.state == 'Leader':
-            msg = self.generate_operation_request_message(is_heartbeat=True)
-            for receiver in self.other_servers:
-                msg[2] = receiver
-                start_new_thread(self.threaded_on_receive_operation, ())
-                start_new_thread(utils.send_message, (tuple(msg), Server.CHANNEL_PORT))
+        for receiver in self.other_servers:
+            msg = self.generate_operation_request_message(receiver, is_heartbeat=True)
+            start_new_thread(self.threaded_on_receive_operation, ())
+            start_new_thread(utils.send_message, (tuple(msg), Server.CHANNEL_PORT))
 
     def start_operation_listener(self):
         # Start listener for operation messages.
@@ -125,7 +151,6 @@ class Server:
         if time.time() - self.last_election_time >= timeout:
             self.last_election_time_lock.release()
             start_new_thread(self.threaded_on_leader_election_timeout, ())
-
 
     def threaded_on_leader_election_timeout(self):
         # Raise self to leader if timeout. Send request for votes. Step down if another leader elected.
