b6956c568236b1e87855c5ea0522cc8414e4f939
Zexi Huang <zexi_huang@ucsb.edu>
2020-11-22 11:11:20 -0800

    + Channel progress.

--- channel.py
+++ channel.py
@@ -13,7 +13,7 @@ import copy
 class Channel:
 
     MAX_CONNECTION = 100
-    BUFFER_SIZE = 1024
+    BUFFER_SIZE = 65536
 
     CHANNEL_PORT = 10000
     CLIENT_PORTS = {
@@ -36,19 +36,44 @@ class Channel:
 
     def __init__(self):
         # Set up the network configurations.
-        # Set up the ports.
-        pass
 
-    def threaded_on_receive(self, connection, ip, port):
+        # Set up the ports.
+        self.port = Channel.CHANNEL_PORT
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((socket.gethostname(), self.port))
+
+        self.start()
+
+    def threaded_on_receive(self, connection):
         # Relay the message from the sender to the receiver.
 
         Channel.network_delay()
-        pass
+        connection.send(pickle.dumps('ACK'))
+        header, sender, receiver, message = pickle.loads(connection.recv(Channel.BUFFER_SIZE))
+
+        # Based on the header and network configuration, decides whether to relay the message.
+        if header in ('Client-Request', 'Client-Feedback'):  # Always relay messages between a client and a server.
+            relay = True
+        else:  # Don't relay messages that involve an isolated server.
+            if self.is_gate_open[sender] and self.is_gate_open[receiver]:
+                relay = True
+            else:
+                relay = False
+
+        if relay:
+            socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if header == 'Client-Request':
+
+
+
 
     def start_message_listener(self):
         # Start the message listener for all incoming messages.
 
-        pass
+        self.socket.listen(Channel.MAX_CONNECTION)
+        while True:
+            connection, (ip, port) = self.socket.accept()
+            start_new_thread(self.threaded_on_receive, (connection, ))
 
     def configuration_change_hanlder(self):
         # Get input from the user to change the network configuration for network partition.
@@ -57,7 +82,9 @@ class Channel:
 
     def start(self):
         # Start the listener for messages and user input handler.
-        pass
+
+        self.start_message_listener()
+        self.configuration_change_hanlder()
 
 
 if __name__ == '__main__':
